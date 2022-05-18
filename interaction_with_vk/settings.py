d106e7019820c237aecc85f1db99f07aef02c0fd
Alexander Danilov <danilov.aa1@mail.ru>
2022-05-18 11:24:29 +0300

    add readme

--- interaction_with_vk/settings.py
+++ interaction_with_vk/settings.py
@@ -1,7 +1,6 @@
 version = '5.131'
 
-token_group = 'e3891ba71be98af88447995d68abebbc0037b3976d3f82c12a612f4f3a3bcfe4b8f3af1062bab0a7e4cfc'
-                #'bc727dca5b9c7d9801b77058a8a16ea9d265c9617a60816e09f12ce4e7c840309e39f2dc8ceefcfcd4d40'
+token_group = ''
 
 access_token = ''
 
